5624643c8603381941e84b6848c0451f1eb687b0
smvenegalla <smvenegalla@adaequare.com>
2017-01-16 15:03:31 +0530

    Work in progress

--- src/app/single-media-player.ts
+++ src/app/single-media-player.ts
@@ -1,13 +1,15 @@
-import {Component} from "@angular/core";
-
+import {Component, ElementRef, OnInit, OnDestroy} from "@angular/core";
 @Component({
     selector: 'single-media-player',
     templateUrl: 'app/single-media-player.html'
 })
-export class SingleMediaPlayer {
+export class SingleMediaPlayer implements OnInit, OnDestroy {
     sources:Array<Object>;
+   private _elementRef: ElementRef
+  private videoJSplayer : VideoJSPlayer
 
-    constructor() {
+    constructor(elementRef: ElementRef) {
+	  this._elementRef = elementRef
         this.sources = [
             {
                 src: "http://static.videogular.com/assets/videos/videogular.mp4",
@@ -22,5 +24,16 @@ export class SingleMediaPlayer {
                 type: "video/webm"
             }
         ];
+    }
+	ngOnInit() {
+        console.log('Init - Component initialized')
+           this.videoJSplayer = videojs(document.getElementById('singleVideo'), {}, function() {
+            // This is functionally the same as the previous example.
+        });
+    }
+
+    ngOnDestroy() {
+        console.log('Deinit - Destroyed Component')
+       
     }
 }
